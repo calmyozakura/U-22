728ebde4504d283d35abd38cb429180abc60b673
i_am_8bit <11807121@stu.kbc.ac.jp>
2020-07-29 14:18:22 +0900

    Revert "ファイル分割的ななにか"
    This reverts commit 7243ab6315d6ad2af4f3047d9e2fad7953d00377.

--- KAZUNA/rise a babble/inertia.cpp
+++ KAZUNA/rise a babble/inertia.cpp
@@ -1,6 +1,17 @@
 #include "DxLib.h"
 #include <math.h>
-#include "inertia.h"
+#define WINDOW_X 480
+#define WINDOW_Y 640
+#define COLOR_BIT 16
+#define DEADZONE 8000
+#define PI 3.14159265358979323846264338f
+#define BULLET_MAX 24
+#define BULLET_SIZE 10
+#define BULLET_SPEED 7
+#define INFINITY_X 24
+#define INFINITY_Y 2
+
+#define DEBUG
 
 
 class Fps {
@@ -44,6 +55,68 @@ public:
 	}
 
 }fps;
+
+//変数宣言
+
+
+float StickX, StickY;
+int infinity[INFINITY_X][INFINITY_Y]{
+	{1,-1},{1,-1},{1,0},{1,0},{1,1},{0,1},{0,1},{-1,1},{-1,0},{-1,0},{-1,-1},{-1,-1},
+	{-1,-1},{-1,-1},{-1,0},{-1,0},{-1,1},{0,1},{0,1},{1,1},{1,0},{1,0},{1,-1},{1,-1}
+};
+
+typedef enum {
+	SET,
+	MAIN
+}state;
+typedef enum {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	VEC_SIZE
+};
+state g_GameState = SET;
+
+struct IMAGES {
+	int muzzle;
+	int player;
+	int back[10];
+	int bubble;
+}images;
+
+struct PLAYER {
+	float x, y;				//座標
+	int size;				//半径
+	int w, h;				//画像の高さ、幅
+	double angle;			//プレイヤーの向き
+	float max_speed;		//プレイヤーの最大速度
+	float scl;
+};
+struct PLAYER player;
+
+struct BULLET {
+	int x, y;
+	int time;
+	double angle;
+	float speed;
+	int c_flg;
+	int m_flg;
+};
+struct BULLET bullet[BULLET_MAX];
+
+struct _VECTOR
+{
+	float Inertia;
+	int Add_Flg;
+	int Add_Cnt;
+	int De_Flg;
+	int De_Cnt;
+};
+struct _VECTOR Vec[VEC_SIZE];
+
+XINPUT_STATE input;
+
 //プロトタイプ宣言
 
 void Main();				//ゲームメイン
@@ -97,7 +170,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 int LoadImages() {
 	if ((images.muzzle = LoadGraph("Images/muzzle.png")) == -1) return -1;
-	if ((images.player = LoadGraph("Images/player_b_.png")) == -1) return -1;
+	if ((images.player = LoadGraph("Images/player___.png")) == -1) return -1;
 	if ((images.bubble = LoadGraph("Images/bubble.png")) == -1) return -1;
 	for (int i = 0; i < 10; i++) {
 		if ((images.back[i] = LoadGraph("Images/stick.png")) == -1) return -1;
@@ -371,8 +444,7 @@ void FloatBubble()
 	static int f=0;
 	static int x = 0, y = 0;
 	f = Cnt(f);
-	if ((Vec->Add_Flg == FALSE && Vec->De_Flg == FALSE) && f%4==0) {
-		
+	if ((Vec->Add_Flg == FALSE && Vec->De_Flg == FALSE) && f%6==0) {
 		player.x += infinity[x][y];
 		y++;
 		player.y += infinity[x][y];
