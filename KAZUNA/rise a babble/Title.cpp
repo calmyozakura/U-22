a30d91cd082eea94f039f269c0a321df35e7903a
i_am_8bit <11807121@stu.kbc.ac.jp>
2020-08-03 12:58:46 +0900

    ファイル分割後(弾が発射できないバグ有)

---
+++ KAZUNA/rise a babble/Title.cpp
@@ -0,0 +1,12 @@
+#include "Scene.h"
+
+
+void Scene::Title() {
+	DrawString(0,0,"Title",0xffffff);
+	DrawString(0, 40, "[Å©] END",0xffffff);
+	DrawString(0, 60, "[Å®] Option", 0xffffff);
+	DrawString(0, 80, "[SPACE] Game Start", 0xffffff);
+	if (input.Buttons[XINPUT_BUTTON_DPAD_LEFT] == TRUE)	 Before = Changer, Changer = ENDING;
+	if (input.Buttons[XINPUT_BUTTON_DPAD_RIGHT] == TRUE) Before = Changer, Changer = OPTION;
+	if (input.Buttons[XINPUT_BUTTON_B] == TRUE)  Before = Changer, Changer = GAMEINIT;
+}
