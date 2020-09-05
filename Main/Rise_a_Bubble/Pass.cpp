1e0470296346a58c0b7559c28d07ac9c3912f6b3
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-09-06 00:25:30 +0900

    パス入力画面でpasteボタンを押すとクリップボードに保存されている文字を張り付け ビルド更新

--- Main/Rise_a_Bubble/Pass.cpp
+++ Main/Rise_a_Bubble/Pass.cpp
@@ -5,12 +5,17 @@ static int Cursor = 0;//Cursor:カーソル用
 bool Ps_OneShot = false, Ps_Flg = false;// OneShot:多重押しの防止 Flg:Bを離すとシーンが変わる　
 bool PBk_OneShot = false, PBk_Flg = false;	//Aボタン(バック)用の多重押し防止
 int p = 0;	//パスの番地をみる
+bool ErrorPass = FALSE;
+char DummyNumber[20];	//パス入力時に使う変数
 void Scene::Pass() {
 
 	DrawGraph(0, 0, images.back[14], FALSE);
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 128);//半透明
-	DrawFillBox(10, 270, WINDOW_X - 10, 430, 0xaaaaaa);
+	DrawFillBox(10, 270, WINDOW_X - 10, 470, 0xaaaaaa);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 255);// 不透明
+
+	SetFontSize(20);
+	if (ErrorPass == TRUE) DrawString(50, 70, "20文字のコードをペーストしてください。", 0xffffff);
 
 	SetFontSize(30);
 	DrawString(50, (WINDOW_Y / 32) * 15, "A", 0xffffff);
@@ -24,7 +29,8 @@ void Scene::Pass() {
 	DrawString(50, (WINDOW_Y / 32) * 19, "I", 0xffffff);
 	DrawString(145, (WINDOW_Y / 32) * 19, "J", 0xffffff);
 	DrawString(240, (WINDOW_Y / 32) * 19, "Back", 0xffffff);
-	DrawString(335, (WINDOW_Y / 32) * 19, "Create", 0xffffff);
+	DrawString(335, (WINDOW_Y / 32) * 19, "Paste", 0xffffff);
+	DrawString(335, (WINDOW_Y / 32) * 21, "Create", 0xffffff);
 
 	DrawTriangle(4 * (7 + (Cursor % 4) * 23), 13 * (23 + (Cursor / 4) * 3),
 		4 * (7 + (Cursor % 4) * 23), 13 * (25 + (Cursor / 4) * 3),
@@ -42,12 +48,12 @@ void Scene::Pass() {
 		PlaySoundMem(se.Sound[choose], DX_PLAYTYPE_BACK);
 	}
 	else if (input.Buttons[XINPUT_BUTTON_DPAD_UP] && Ps_OneShot == false) {
-		(Cursor <= 3) ? Cursor += 8 : Cursor -= 4;
+		(Cursor <= 3) ? Cursor += 12 : Cursor -= 4;
 		Ps_OneShot = true;
 		PlaySoundMem(se.Sound[choose], DX_PLAYTYPE_BACK);
 	}
 	else if (input.Buttons[XINPUT_BUTTON_DPAD_DOWN] && Ps_OneShot == false) {
-		(Cursor >= 8) ? Cursor -= 8 : Cursor += 4;
+		(Cursor >= 12) ? Cursor -= 12 : Cursor += 4;
 		Ps_OneShot = true;
 		PlaySoundMem(se.Sound[choose], DX_PLAYTYPE_BACK);
 	}
@@ -67,7 +73,18 @@ void Scene::Pass() {
 			}
 		}
 		else if (Cursor == 10)PlaySoundMem(se.Sound[cancel], DX_PLAYTYPE_BACK), Cursor = 0, Before = Changer, Changer = GAMEMODE;
-		else if (Cursor == 11) CodeRnd_flg = FALSE, Pass_Flg = TRUE, Load_Flg = FALSE, Before = Changer, Changer = GAMEINIT;		//難易度選択
+		else if (Cursor == 11) {
+			GetClipboardText(DummyNumber);	//ダミー変数でクリップボードの文字数が条件を満たしているか調べる
+			if (DummyNumber[19] == NULL || DummyNumber[20] != NULL) { //20文字未満or21文字以上でエラー文表示
+				ErrorPass = TRUE;
+			}
+			else {
+				ErrorPass = FALSE;
+				GetClipboardText(PassNumber);
+				p = 20;
+			}
+		}
+		else if (Cursor == 15) CodeRnd_flg = FALSE, Pass_Flg = TRUE, Load_Flg = FALSE, Before = Changer, Changer = GAMEINIT;	//難易度選択
 
 		Ps_Flg = false;
 	}
